9ff69e1fe08f4ebc97102d91b3f2f1485422100b
Aman Eleven <hackerr647@gmail.com>
2022-01-22 21:14:24 +0530

    Create Print the Elements of a Linked List.java

---
+++ Linked Lists/Print the Elements of a Linked List.java
@@ -0,0 +1,21 @@
+
+
+//write this after  this given line -->     static void printLinkedList(SinglyLinkedListNode head) {
+
+
+
+
+if(head == null)
+        return;
+        
+        while (head!=null)
+        {
+            System.out.println(head.data);
+            head=head.next;
+        }
+      return;
+      
+      
+    
+//Author : AmanEleven
+//Follow : All Personal and Professional Links Here -->  https://linktr.ee/amaneleven
